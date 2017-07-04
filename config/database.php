d5268b23132d0d507667b0ecffa4c315b39f64d6
MaTiUs77 <matius77@gmail.com>
2017-07-04 15:01:43 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => 'localhost',
             'database'  => 'finanzas',
             'username'  => 'finanzas',
-            'password'  => 'MysqlFinanzas',
+            'password'  => 'futur0',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
